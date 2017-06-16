e31ae876902c8a5e58634e966070e5a33339c065
Crystal <hello@crystalbohacek.com>
2017-06-16 13:17:11 -0400

    added bootstrap; changed html structure

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
   
-  var twitchStreamers = ["sethbling","wekeepsitreal", "kociqq", "twilliams71", "freecodecamp", "techlahoma", "HexwellC"];
+  var twitchStreamers = ["kojaktsl", "Serpent_AI", "HardlyDifficult", "windybeardgames", "drunkdevs", "numfin", "xcodeblocks", "vFujin", "sethbling", "wekeepsitreal", "kociqq", "twilliams71", "freecodecamp", "techlahoma", "HexwellC"];
+        
+for (var i = 0; i < twitchStreamers.length-1; i++) {
+        ajax();
+}
   
-      var url = "https://wind-bow.glitch.me/twitch-api/streams/" + twitchStreamers[0] + "?callback=?"
-      console.log(url)
-  $.ajax({
-    url: url,
+ function ajax(){ 
+   $.ajax({
+    url: "https://wind-bow.glitch.me/twitch-api/streams/" + twitchStreamers[i] + "?callback=?",
     success:function(result){
-      
       var data = JSON.parse(result.substring(32,result.length-2)); 
-      $("p").text("Name: " + data.stream.channel.display_name)
 
-      if (result){}
+      if (data.stream){
+        var output = "";
+        for (i = 0; i <= 4; i++){
+          output += '<br><li><h3>' + data.stream.channel.display_name + '</h3></li>'
+        }
+        $("#output").html(output)
+        
+      } else {
+        $("p").text("User Offline");
+      }
 
     },
     error: function(errorMessage){
           alert("error", errorMessage);
     }
   })
+ }
 });
