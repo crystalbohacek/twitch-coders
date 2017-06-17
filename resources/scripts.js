2cd03d81fe8b5dce3cd029da0d7aa11e984faa93
Crystal <hello@crystalbohacek.com>
2017-06-17 12:39:12 -0400

    fixed the data loop

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
   
-  var twitchStreamers = ["kojaktsl", "Serpent_AI", "HardlyDifficult", "windybeardgames", "drunkdevs", "numfin", "xcodeblocks", "vFujin", "sethbling", "wekeepsitreal", "kociqq", "twilliams71", "freecodecamp", "techlahoma", "HexwellC"];
-        
-for (var i = 0; i < twitchStreamers.length-1; i++) {
-        ajax();
-}
+  var twitchStreamers = ["kojaktsl", "Serpent_AI", "HardlyDifficult", "windybeardgames", "drunkdevs", "numfin", "xcodeblocks", "vFujin", "sethbling", "wekeepsitreal", "kociqq", "twilliams71", "freecodecamp", "techlahoma", "HexwellC", "CultOfRig", "JackOfSporks"];
+  var output = "";
+  var $outputEl = $("#output");
+  var $offlineOutputEl = $("#offline-output");
+
+
+  for (var i = 0; i < twitchStreamers.length-1; i++) {
+    ajax(twitchStreamers[i]);
+  }
   
- function ajax(){ 
+  function ajax(twitchStreamer){ 
    $.ajax({
-    url: "https://wind-bow.glitch.me/twitch-api/streams/" + twitchStreamers[i] + "?callback=?",
+    url: "https://wind-bow.glitch.me/twitch-api/streams/" + twitchStreamer + "?callback=?",
     success:function(result){
       var data = JSON.parse(result.substring(32,result.length-2)); 
 
       if (data.stream){
-        var output = "";
-        for (i = 0; i <= 4; i++){
-          output += '<br><li><h3>' + data.stream.channel.display_name + '</h3></li>'
-        }
-        $("#output").html(output)
-        
+        output = '<li><h3>' + data.stream.channel.display_name + '</h3></li>'
+        $outputEl.html($outputEl.html() + output);
       } else {
-        $("p").text("User Offline");
+        output = '<li><p>' + twitchStreamer + ' is offline </p></li>'
+        $offlineOutputEl.html($offlineOutputEl.html() + output);
       }
-
     },
     error: function(errorMessage){
           alert("error", errorMessage);
