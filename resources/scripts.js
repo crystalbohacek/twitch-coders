2a76384b43c4d23737cf0060ad317b04b5cda26d
Crystal <hello@crystalbohacek.com>
2017-06-19 00:41:02 -0400

    added css; displayed online users at the top

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,17 +1,53 @@
 $(document).ready(function() {
   
-  var twitchStreamers = ["kojaktsl", "Serpent_AI", "HardlyDifficult", "windybeardgames", "drunkdevs", "numfin", "xcodeblocks", "vFujin", "sethbling", "wekeepsitreal", "kociqq", "twilliams71", "freecodecamp", "techlahoma", "HexwellC", "CultOfRig", "JackOfSporks"];
-  var output = "";
-  var $outputEl = $("#output");
-  var $offlineOutputEl = $("#offline-output");
+  var twitchStreamers = [
+        "kojaktsl",
+        "Serpent_AI",
+        "HardlyDifficult",
+        "windybeardgames",
+        "drunkdevs",
+        "numfin",
+        "xcodeblocks",
+        "vFujin",
+        "sethbling",
+        "wekeepsitreal",
+        "kociqq",
+        "twilliams71",
+        "freecodecamp",
+        "techlahoma",
+        "HexwellC",
+        "CultOfRig",
+        "JackOfSporks",
+        "hustle_man"
+  ];
 
+var globalStartIndex = 0;
+var globalEndIndex = 4;
+  
+  
+var output = "";
+var $outputEl = $("#output");
+var $offlineOutputEl = $("#offline-output");
+var $loadMoreButton = $("#load-more");
 
-  for (var i = 0; i < twitchStreamers.length-1; i++) {
+  
+function loadStreamers(){
+  if (globalEndIndex >= twitchStreamers.length){
+    globalEndIndex = twitchStreamers.length;
+    $loadMoreButton.hide();
+  }
+
+  for (var i = globalStartIndex; i < globalEndIndex; i++) {
     ajax(twitchStreamers[i]);
   }
-  
-  function ajax(twitchStreamer){ 
-   $.ajax({
+
+  globalStartIndex = globalEndIndex + 1;
+  globalEndIndex = globalStartIndex + 5;
+}
+
+function ajax(twitchStreamer){ 
+  $loadMoreButton.text('Loading...');
+  $.ajax({
     url: "https://wind-bow.glitch.me/twitch-api/streams/" + twitchStreamer + "?callback=?",
     success:function(result){
       var data = JSON.parse(result.substring(32,result.length-2)); 
@@ -23,10 +59,47 @@ $(document).ready(function() {
         output = '<li><p>' + twitchStreamer + ' is offline </p></li>'
         $offlineOutputEl.html($offlineOutputEl.html() + output);
       }
+      $loadMoreButton.text('Load more');
+      
     },
     error: function(errorMessage){
           alert("error", errorMessage);
     }
   })
- }
+}    
+  
+  $('#load-more').on('click', function(){
+    loadStreamers()
+  });
+  
+  
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.twitch.tv/kraken/streams/communities/programming?game=Creative',
+    success: function(data) {
+      data = JSON.parse(data);
+
+      var streams = data.streams;
+      var liveStreams = [];
+      var indexOfNewStream;
+
+      for (var i = 0, j = streams.length; i < j; i++){
+        newStream = streams[i].channel.name;
+        indexOfNewStream = twitchStreamers.indexOf(newStream);
+        if (indexOfNewStream > -1){
+          twitchStreamers.splice(indexOfNewStream, 1)          
+        }
+        twitchStreamers.unshift(newStream);
+      }
+      console.log(twitchStreamers);
+      
+
+//      for (var i = 0; i < twitchStreamers.length-1; i++) {
+//        ajax(twitchStreamers[i]);
+//      }
+      
+      loadStreamers();
+          
+    }
+  });
 });
